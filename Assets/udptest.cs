17a09eeb618f919a80a4d28e9db99eb09a8063bf
devemin <mahsand@nifty.com>
2020-11-08 02:31:36 +0900

    float & Int32 対応
    2nd update.

--- Assets/udptest.cs
+++ Assets/udptest.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System;
+using UnityEngine.Experimental.GlobalIllumination;
 
 //Unity でUDP通信受信
 //https://qiita.com/nenjiru/items/8fa8dfb27f55c0205651
@@ -23,8 +25,12 @@ public class udptest : MonoBehaviour
 
     Thread thread;
 
+    const int datanum = 9;
+    static float[] f_buf = new float[datanum];
+    static Int32[] i_buf = new Int32[datanum];
 
-    string host = "192.168.0.33";
+
+    string host = "192.168.0.62";
     int port = 22224;
 
     // Start is called before the first frame update
@@ -41,7 +47,9 @@ public class udptest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //アクセス例
+        //Debug.Log(i_buf[1].ToString());
+        //Debug.Log(f_buf[2].ToString());
     }
     void OnApplicationQuit()
     {
@@ -51,31 +59,43 @@ public class udptest : MonoBehaviour
 
     private static void ThreadMethod()
     {
+        String test;
+
         while (true)
         {
             IPEndPoint remoteEP = null;
             byte[] data = udp.Receive(ref remoteEP);
-            string test = "";
-            test = "0x";
+            //Debug.Log(data.Length.ToString());
 
-            for (int a = data.Length - 1; a >= 0; a--)
+            //パケットサイズが正しければ処理開始
+            if (data.Length == datanum * 4 ) // float または Int32 のサイズとして 4
             {
-                test += data[a].ToString("x2");
+                test = "";
+
+                for (int a = 0; a<datanum; a++)
+                {
+                    //i_buf[a] = BitConverter.ToInt32(data, a * sizeof(Int32) );
+                    //test += i_buf[a].ToString() + ", ";
+
+                    f_buf[a] = BitConverter.ToSingle(data, a * sizeof(float) );
+                    test += f_buf[a].ToString() + ", ";
+                }
+                Debug.Log(test);
             }
-            Debug.Log(test);
-
-            //連続したバイトを2byte や4byte のデータタイプ(int や float など)に変換するときは、ビットシフトなど利用
-            // float newdata = (data[0] << 8) & (data[1]);
-
         }
     }
 
 
     public void senddataUDP()
     {
+        //4byte だけテストで送る
+        //0x36353433 = 909456435
+        //送るバイト順の、処理順に注意
+        //送信成功したら、ESP32 のシリアルモニタで、909456435と表示されます。
+        byte[] data = { 0x33, 0x34, 0x35, 0x36};
+        udp_send.Send(data, 4);
 
-        byte[] data = { 0x33, 0x34, 0x35, };
-        udp_send.Send(data, 3);
+        Debug.Log("Data send! check your ESP32 serial monitor.");
     }
 
 }
--- udptest.ino
+++ udptest.ino
@@ -1,96 +1,150 @@
+//↓M5StickC ならこれを有効にし、Setup() でM5.begin() を行う
+//#include <M5StickC.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-const char* ssid = "SSID";
-const char* password = "passpass";
-
-const char* client_address = "192.168.0.13";  //送り先
-const int client_port = 22222;  //送り先
-const int server_port = 22224;  //このESP32 のポート番号
-
 
 //ESP32 x Unity x UDP
-//3byte UDPにて送受信するサンプルです。
+//WiFi UDPにて送受信するサンプルです。
 
 //参考：
 //ESP32でUDP通信やってみた（ESP32インストール手順解説あり）
 //https://qiita.com/Tsukkey/items/247285c703fbbc6c6cd2
-
 //M5Stack同士でWiFi, UDPによる双方向リアルタイム同時通信する実験
 //https://www.mgo-tec.com/blog-entry-udp-wifi-m5stack.html/3#title09
+//２バイト以上のデータタイプはビットシフトしてバイト分割して送る、float は共用体を利用して送る
+//https://hawksnowlog.blogspot.com/2016/11/sending-multibytes-with-serialwrite.html#float-4byte-%E3%81%AE%E6%83%85%E5%A0%B1%E3%82%92%E9%80%81%E4%BF%A1%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95
 
+const char* ssid = "apname";
+const char* password = "appassword";
+
+const char* client_address = "192.168.0.33";  //送り先
+const int client_port = 22222;  //送り先
+const int server_port = 22224;  //このESP32 のポート番号
+
+
+#define RECVDATANUM 9
+#define SENDDATANUM 9
 
 WiFiUDP udp;
-boolean sendflag = false;
 
-byte recvbuf[1024];
-byte sendbuf[1024];
-int recvbuf_size = 3;
-int sendbuf_size = 3;
+//データ送受信のfloat のやり取りの為に、共用体を用いる。
+//共用体は、下記宣言で4byte のバイナリデータなのに、.ival や .fval 等とアクセスすることで中身を int や float に読み替えられる機能
+typedef union {
+  int32_t ival;
+  float fval;
+  byte binary[4];
+} uf;
 
-int c=0;
+
+uf r_ufdata[RECVDATANUM];
+uf s_ufdata[SENDDATANUM];
+
+
 
 
 void setup() {
   Serial.begin(115200);
 
+  //M5StickC の時は実行
+  //M5.begin();
+
+  //WiFi 初期化
   Serial.println("[ESP32] Connecting to WiFi network: " + String(ssid));
   WiFi.disconnect(true, true);
   delay(500);
-  
   WiFi.begin(ssid, password);
   while( WiFi.status() != WL_CONNECTED) {
     delay(500);  
   }  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  //UDP 開始
   udp.begin(server_port);
   delay(500);  
 
-  randomSeed(1);
 }
 
 
+//受信
+//パケットが来ていれば、RECVDATANUM 分だけr_ufdata配列に保存する
 void receiveUDP(){
     int packetSize = udp.parsePacket();
-    
-    if(packetSize > 0){
+    byte tmpbuf[128];
+
+    //今回はUnity にここに来る処理は入れてないが、RECVDATANUM 分のfloatが来た場合の処理　の想定
+    if(packetSize == RECVDATANUM){
       //Serial.println("recv");
-      int getsize = udp.read(recvbuf, recvbuf_size);
-      Serial.print(recvbuf[0], HEX);
-      Serial.print(recvbuf[1], HEX);
-      Serial.print(recvbuf[2], HEX);
-      Serial.print("\n");
+      for (int a=0; a<RECVDATANUM; a++) 
+      {
+        udp.read(tmpbuf, sizeof(uf));
+        for (int b=0; b<sizeof(uf); b++) 
+        {
+          r_ufdata[a].binary[b] = tmpbuf[a*sizeof(uf) + b];
+        }
+        //Serial.println("recv: " + String(r_ufdata[a].ival) );
+        Serial.println("recv: " + String(r_ufdata[a].fval) );
+      }
+    }
+    //Unity のテストボタンで４バイトのデータが送られてきた時の処理    
+    else if(packetSize == 4)
+    {
+      //Serial.println("recv");
+      udp.read(tmpbuf, sizeof(uf));
+      for (int a=0; a<sizeof(uf); a++) 
+      {
+        r_ufdata[0].binary[a] = tmpbuf[a];
+      }
+      Serial.println("recv: " + String(r_ufdata[0].ival) );
     }
 }
 
+
+//送信
+//SENDDATANUM 分送る
 void sendUDP(){
-  if(sendflag){
-    //Serial.println("send");    
-    udp.beginPacket(client_address, client_port);
-    udp.write(sendbuf, sendbuf_size);
-    udp.endPacket();
-    sendflag = false;
-  }
+  String test = "";
   
-  //２バイト以上のデータタイプはビットシフトしてバイト分割して送る
-  //float は共用体を利用して送る
-  //https://hawksnowlog.blogspot.com/2016/11/sending-multibytes-with-serialwrite.html#float-4byte-%E3%81%AE%E6%83%85%E5%A0%B1%E3%82%92%E9%80%81%E4%BF%A1%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95
+  //Serial.println("send");    
+  udp.beginPacket(client_address, client_port);
+
+  for (int a=0; a<SENDDATANUM; a++) {
+    udp.write(s_ufdata[a].binary, sizeof(uf) );
+    //test += String(s_ufdata[a].ival) + ", ";
+    test += String(s_ufdata[a].fval) + ", ";
+  }
+  Serial.println("send: " + test );
+  
+  udp.endPacket();
 }
 
  
 void loop() {
+
+  //もしデータパケットが来ていれば受信する
   receiveUDP();
 
 
-  if (c>= 10) {
-    sendflag = true;
-    sendbuf[0] = random(0,255);
-    sendbuf[1] = random(0,255);
-    sendbuf[2] = random(0,255);
-
-    sendUDP();
-    c = 0;
+  //テストデータとして、0.0 ～ 0.5 のデータ値を作って送る
+  //たとえばジャイロやセンサのデータを少数のまま送信できます。
+  for (int c=0; c<SENDDATANUM; c++) {
+    s_ufdata[c].fval = ( 0.1 * (float)c );
   }
-  delay(100);
-  c++;
+
+  //こちらはint データの送信例
+  /*
+  for (int c=0; c<SENDDATANUM; c++) {
+    s_ufdata[c].ival = c;
+  }
+  */
+    
+  sendUDP();
+
+
+  delay(500);
   
 }
